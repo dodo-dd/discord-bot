5de736e73309b185808f5762c8b1d33cd228f721
dodo-dd <81002515+dodo-dd@users.noreply.github.com>
2021-10-30 16:48:09 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+const Discord=require('discodrd.js')
+const client=new Discord.Client()
+const {prefix, token} = require('./config.json')
+const fs = require('fs')
+
+client.commands = new Discord.Collection()
+const commandsFiles = fs.readdirSync('./commands').fiter(file => file.endsWith('.js'))
+
+for(const file of xommandsFiles){
+    const command = require('./commands/${file}')
+    client.commands.set(command.name,command)
+}
+
+client.on('Ready',()=>{
+    console.log("봇이 준비되었습니다.")
+})
+client.on('message', msg=>{
+    if(!msg.content.starsWith(prefix) || msg.author.bot) return
+    const args = msg.content.slice(prefix.length).trim().split(/ +/)
+    const commandName = args.shift()
+    const command = client.commands.get(commandName) 
+    try{
+        command.exeute(msg,args)
+    }catch(error){
+        console.log(error)
+    }
+})
+
+client.login('token')
